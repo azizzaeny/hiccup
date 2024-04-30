1447e10f854aa212d5589d4e9c63fd5520de6123
azizzaeny <zie.azlance@gmail.com>
2024-04-30 10:44:55 +0700

    add first dist es6

---
+++ dist/index.js
@@ -0,0 +1,65 @@
+var createElement = (hnode) => {
+  if(typeof hnode === "string") return document.createTextNode(hnode);
+  let [tagName, attrs, ...children] = hnode;
+  /*create node*/
+  let elementNode = document.createElement(tagName);
+
+  /*create attribute*/
+  if(attrs === null){ attrs = {} };
+  if(typeof attrs === "String"){
+    attrs = {"class": attrs};
+  };
+  
+  /*functions event attribute*/
+  for (let attrName in attrs){
+    if(attrName.startsWith('on') && typeof attrs[attrName] === 'function'){
+      let eventName = attrName.substring(2);
+      elementNode.addEventListener(eventName, attrs[attrName]);
+    } else {
+      elementNode.setAttribute(attrName, attrs[attrName]);
+    }
+  }
+
+  /*create children */
+  if(typeof children === "string"){
+    elementNode.appendChild(createElement(children));
+    return elementNode;
+  }
+  for(let child of children){
+    elementNode.appendChild(createElement(child));
+  }
+  
+  return elementNode;
+
+}
+
+var render = (container, hnode) =>{
+  container.innerHTML= ''; // remove from mem
+  container.appendChild(createElement(hnode));
+  return true;
+}
+
+var toString = (hnode) => {
+  //TODO
+}
+
+var toHiccup = (htmlString) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlString, 'text/html');
+  function parseNode(node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+      return node.textContent.trim() !== '' ? node.textContent.trim() : null;
+    }
+    
+    const attributes = {};
+    for (const attr of node.attributes) {
+      attributes[attr.name] = attr.value;
+    }
+    
+    const children = Array.from(node.childNodes).map(parseNode).filter(Boolean);
+    
+    return [node.tagName.toLowerCase(), attributes, ...children];
+  }  
+  const root = doc.body.firstChild;
+  return parseNode(root);
+}
---
+++ readme.md
@@ -0,0 +1,51 @@
+## @zaeny/hiccup
+> UI building with hiccup
+Functional Programming UI with hiccup 
+
+- [Geting Started](#getting-started)
+- [Usage](#usage)
+- [API](#api)
+
+### Getting started 
+(status: work in progress)  
+**in browser** global vars 
+
+```js
+<script src="https://cdn.jsdelivr.net/gh/azizzaeny/hiccup@main/dist/index.js"></script>
+```
+
+**in browser es6**
+```html
+<script src="https://cdn.jsdelivr.net/gh/azizzaeny/hiccup@main/dist/index.es6.js"></script>
+```
+or 
+```js
+var deps = {
+  hiccup : "https://cdn.jsdelivr.net/gh/azizzaeny/hiccup@main/dist/index.es6.js",
+}
+
+var assignVar = (global) => res => Object.assign(global, res.default );
+
+import(deps.hiccup).then(assignVar(window));
+```
+
+### usage
+
+```js
+var component = (props) => ["div", "container", ["div", {class: "w-10"}, "look iam hiccup"]];
+
+render(document.body,  component());
+
+
+/*
+ toHiccup('<div>hai</div>');
+*/
+```
+
+### Api
+```js
+createElement(hnode),
+render(container, hnode)
+toHiccup(str)
+toString(hnode) 
+```
